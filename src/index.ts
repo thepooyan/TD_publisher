9cc2c92758304d6d8c60f8f27502a94018d6e9f4
pooyan <thepooyan@gmail.com>
2025-10-05 10:42:23 +0330

    nothing!

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-import { pause } from "./util";
+import { spawn } from "child_process";
 
-console.log("Hello via Bun!");
+const vsDevCmd = `C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise\\Common7\\Tools\\VsDevCmd.bat`;
+const project = "TahlildadehMvc.csproj";
+const profile = "FolderProfile";
+const command = `"${vsDevCmd}" && msbuild "${project}" /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=${profile}`;
+
+async function run() {
+  await new Promise<void>((resolve, reject) => {
+    const cmd = spawn(command, { // Pass the whole command here
+      stdio: "inherit",
+      shell: true // And use shell: true
+    });
+
+    cmd.on("exit", (code) => {
+      if (code === 0) resolve();
+      else reject(new Error(`Process exited with code ${code}`));
+    });
+
+    cmd.on("error", (err) => {
+      reject(err);
+    });
+  });
+}
+
+run()
+  .then(() => {
+    console.log("Build and publish finished!");
+  })
+  .catch((err) => console.error("Error:", err.message));
