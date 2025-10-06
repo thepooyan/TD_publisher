aa4830fdd77a71ac86454215fe51cfd327f6eff2
pooyan <thepooyan@gmail.com>
2025-10-06 13:12:47 +0330

    nothing!

--- src/lib/pubManager.ts
+++ src/lib/pubManager.ts
@@ -1,8 +1,9 @@
 import { loadConfig } from "./config";
 import path from "path"
 import { STATIC } from "./const";
-import { readdirSync } from "fs";
+import { Dirent, readdirSync } from "fs";
 import { prompt, type askQ } from "./prompt";
+import { log } from "console";
 
 let publishProfileCache:string | null = null;
 
@@ -15,12 +16,20 @@ const selectPublishProfile = async () => {
     const config = await loadConfig()
     const pubPath = path.join(config.projectPath, STATIC.pubProfilesPath)
     const files = readdirSync(pubPath, {withFileTypes: true})
-    console.log(pubPath)
+    const profilesMap = new Map<number, string>()
+    let index = 1
     for (const file of files) {
         if (path.extname(file.name) !== ".pubxml") continue
-        console.log(file.name)
+        log(`${index++}: ${file.name}`)
+        const fullPath = path.join(pubPath, file.name)
+        profilesMap.set(index, fullPath)
     }
-    let a = await prompt("which profile to use? (enter the number)\n")
-    console.log(a)
+    let trg = null
+    do {
+        let res = await prompt("which profile to use? (enter the number)")
+        trg = profilesMap.get(parseInt(res))
+    } while(!trg)
+    console.log(trg)
+        
     return ""
 }
